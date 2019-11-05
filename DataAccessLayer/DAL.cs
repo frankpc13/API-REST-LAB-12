de6209a78fbe5f23627739d595313164557ca344
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2019-11-04 19:33:46 -0500

    Add project files.

---
+++ DataAccessLayer/DAL.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public static class DAL
+    {
+        static APIDBEntities1 DbContext;
+
+        static DAL()
+        {
+            DbContext = new APIDBEntities1();
+        }
+
+        public static List<Product> GetAllProducts()
+        {
+            return DbContext.Products.ToList();
+        }
+
+        public static Product GetProduct(int productID)
+        {
+            return DbContext.Products.Where(x => x.ProductId == productID).FirstOrDefault();
+        }
+
+        public static bool InsertProduct(Product product)
+        {
+            bool status;
+            try
+            {
+                DbContext.Products.Add(product);
+                DbContext.SaveChanges();
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public static bool UpdateProduct(Product product)
+        {
+            bool status = false;
+            try
+            {
+                Product NewProduct = DbContext.Products.Where(x => x.ProductId == product.ProductId).FirstOrDefault();
+                if (NewProduct != null)
+                {
+                    NewProduct.ProductName = product.ProductName;
+                    NewProduct.Quantity = product.Quantity;
+                    NewProduct.Price = product.Price;
+                    DbContext.SaveChanges();
+                }
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public static bool DeleteProduct(int id)
+        {
+            bool status = false;
+
+            try
+            {
+                Product product = DbContext.Products.Where(x => x.ProductId == id).FirstOrDefault();
+                if(product != null)
+                {
+                    DbContext.Products.Remove(product);
+                    DbContext.SaveChanges();
+                }
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+    }
+}
---
+++ WebAPISERVICE2/Controllers/ShowRoomController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPISERVICE2.Repository;
+using System.Web.Http.Results;
+using DataAccessLayer;
+
+namespace WebAPISERVICE2.Controllers
+{
+    public class ShowRoomController : ApiController
+    {
+        [HttpGet]
+        public JsonResult<List<Models.Product>> GetAllProduct()
+        {
+            EntityMapper<DataAccessLayer.Product, Models.Product> mapObject = new EntityMapper<DataAccessLayer.Product, Models.Product>();
+            List<DataAccessLayer.Product> productList = DAL.GetAllProducts();
+            List<Models.Product> products = new List<Models.Product>();
+            foreach(var item in productList)
+            {
+                products.Add(mapObject.Translate(item));
+            }
+            return Json<List<Models.Product>>(products);
+        }
+
+        [HttpPost]
+        public bool InsertProduct(Models.Product product)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                EntityMapper<Models.Product, DataAccessLayer.Product> mapObject = new EntityMapper<Models.Product,  DataAccessLayer.Product>();
+                DataAccessLayer.Product productObject = new DataAccessLayer.Product();
+                productObject = mapObject.Translate(product);
+                status = DAL.InsertProduct(productObject);
+            }
+            return status;
+        }
+
+        [HttpPut]
+        public bool UpdateProduct(Models.Product product)
+        {
+            EntityMapper<Models.Product, DataAccessLayer.Product> mapper = new EntityMapper<Models.Product, DataAccessLayer.Product>();
+            DataAccessLayer.Product productObj = new DataAccessLayer.Product();
+            productObj = mapper.Translate(product);
+            var status = DAL.UpdateProduct(productObj);
+            return status;
+        }
+
+        [HttpDelete]
+        public bool DeleteProduct(int id)
+        {
+            var status = DAL.DeleteProduct(id);
+            return status;
+        }
+    }
+}
